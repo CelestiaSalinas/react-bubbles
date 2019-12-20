e51e44bd21b6184c14119fb469738de8d7125706
Celestia Salinas <csalinas3671@gmail.com>
2019-12-20 13:47:08 -0500

    prog

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { useState } from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth'
+// import { withFormik, Form, Field } from 'formik'
+// import * as Yup from "yup";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
 
-export default Login;
+function Login(props) {
+    const [user, setUser] = useState({ username: '', password: '' })
+
+    const handleChange = event => {
+        setUser({ ...user, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = e => {
+        console.log(user)
+        e.preventDefault();
+        axiosWithAuth()
+            .post(`/api/login`, user)
+            .then(response => {
+                console.log(response)
+                localStorage.setItem('token', response.data.payload)
+                props.history.push('/')
+            })
+            .catch(error => {
+                console.log(error.response)
+            })
+    }
+
+    return (
+        <div className='container'>
+            <h1 className='login-header'>Login</h1>
+            <form className='customForm' onSubmit={handleSubmit}>
+                <label className='label' htmlFor='username'> Username: </label>
+                <input
+                    className='input'
+                    type='text'
+                    name='username'
+                    placeholder='Username'
+                    value={user.username}
+                    onChange={handleChange}
+                    required
+                />
+
+                <label className='label' htmlFor='password'> Password: </label>
+                <input
+                    className='input'
+                    type='password'
+                    name='password'
+                    placeholder='Password'
+                    value={user.password}
+                    onChange={handleChange}
+                    required
+                />
+                <button onClick={handleSubmit} className='submitButton' type='submit'>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
